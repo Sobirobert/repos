542e5c745164c1ece2685829560000775e4370f8
Sobirobert <rsobierajpsp@gmail.com>
2023-09-29 17:09:21 +0200

    Praca ze wzorcem repozytorium

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,32 +1,8 @@
-﻿using MotoApp;
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
 
-var stack = new BasicStack<double>();
-var stackString = new BasicStack<string>();
-stack.Push(4.5);
-stack.Push(43);
-stack.Push(333.6);
-
-stackString.Push("Company");
-stackString.Push("Company2");
-stackString.Push("Company3");
-
-
-double sum = 0.0;
-string sumInt = null;
-
-while (stack.Count > 0)
-{
-    double item = stack.Pop();
-    Console.WriteLine($"Item: {item}");
-    sum += item;
-}
-
-while (stackString.Count > 0)
-{
-    string item2 = stackString.Pop();
-    Console.WriteLine($"Item: {item2}");
-    sumInt += item2;
-}
-
-Console.WriteLine($"Sum: {sum}");
-Console.WriteLine($"Sum: {sumInt}");
+var employeeRepository = new EmployeeRepository();
+employeeRepository.Add(new Employee { FirstName = "Adam" });
+employeeRepository.Add(new Employee { FirstName = "Piotr" });
+employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+employeeRepository.Save();
