a668fee7c3bab5c4a2c3f8b7a9556d3b1d2b52b4
Sobirobert <144240542+Sobirobert@users.noreply.github.com>
2023-09-30 12:00:54 +0200

    Delete ConsoleApp directory

--- ConsoleApp/ConsoleApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using MotoApp.Entities;
-using MotoApp.Repositories;
-
-var employeeRepository = new EmployeeRepository();
-employeeRepository.Add(new Employee { FirstName = "Adam" });
-employeeRepository.Add(new Employee { FirstName = "Piotr" });
-employeeRepository.Add(new Employee { FirstName = "Zuzia" });
-employeeRepository.Save();
